cf93f46e0f2fb7d9f25970cc9f17cc463c880c55
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2016-02-11 10:35:15 -0500

    Adding buffer around country border to ensure complete coverage

--- us-geospatial-filter/grails-app/conf/BootStrap.groovy
+++ us-geospatial-filter/grails-app/conf/BootStrap.groovy
@@ -11,7 +11,7 @@ class BootStrap {
 		// load the US boundary geometry
 		def file = getClass().getResource("/us_boundaries.geojson")
 		def json = new JsonSlurper().parseText(file.getText())
-		grailsApplication.config.usBoundaries = new MultiPolygon(json.geometry.coordinates)
+		grailsApplication.config.usBoundaries = new MultiPolygon(json.geometry.coordinates).buffer(0.1)
 	}
 
 	def destroy = {}
